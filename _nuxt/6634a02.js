7871d72970645cfff6ef26950f56dafb264434f3
vyvydy <aengbeol@gmail.com>
2022-02-20 18:17:58 +0900

    어부 정보 추가

--- _nuxt/6634a02.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{337:function(t,e,n){"use strict";n.r(e);var l=n(63),component=Object(l.a)({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("section",{staticClass:"layout-headline"},[t._t("headline"),t._v(" "),t._t("content")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
